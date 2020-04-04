3a1eacf2a54bec433d74e35c79f2a6629f881e88
“yaops <243241005@qq.com>
2020-04-04 22:21:05 +0800

    第一天更新，初步搭建项目

---
+++ gmall-user/src/main/java/com/yaoit/gmall/user/GmallUserApplication.java
@@ -0,0 +1,16 @@
+package com.yaoit.gmall.user;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@MapperScan(basePackages = "com.yaoit.gmall.user.mapper")
+public class GmallUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallUserApplication.class, args);
+    }
+
+}
